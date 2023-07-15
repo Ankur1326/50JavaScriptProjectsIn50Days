7450101cf9e91c2efc822a51e0aaca1737d01d72
Ankur1326 <swamiankur46@gmail.com>
2023-07-15 17:30:47 +0530

    update script.js file

--- incrementing-counter/script.js
+++ incrementing-counter/script.js
@@ -1,18 +1,19 @@
-const counters = document.querySelectorAll(".counter")
+const counters = document.querySelectorAll(".counter") 
 
-counters.forEach(counter => {
-    counter.innerText = '0';
+// For Each counter element
+counters.forEach(counter => { 
+    counter.innerText = '0'; // set the initial value
 
     const updateCounter = () => {
-        const target = +counter.getAttribute('data-target')
-        const c = +counter.innerText
+        const target = +counter.getAttribute('data-target') // Get the target value from the data-target attribute
+        const c = +counter.innerText // Get the current value
 
         const increment = target / 200
         console.log(increment);
 
         if (c < target) {
-            counter.innerText = `${Math.ceil(c + increment)}`
-            setTimeout(updateCounter, 1);
+            counter.innerText = `${Math.ceil(c + increment)}` //Update the text inside the counter element with the rounded-up value of the current value plus the increment
+            setTimeout(updateCounter, 1); // calling the updatecounter again and again based on 1ms duration
         } else {
             counter.innerText = target
         }
