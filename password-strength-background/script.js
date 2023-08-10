13c97395749f51bde488706f4d1a601c07135976
Ankur1326 <swamiankur46@gmail.com>
2023-08-10 22:53:10 +0530

    add sticky-navgation and update p-s-b

--- password-strength-background/script.js
+++ password-strength-background/script.js
@@ -1,9 +1,9 @@
-const password = document.getElementById('passowrd')
-const background = document.getElementById("background")
+const password = document.getElementById('password')
+const background = document.getElementById('background')
 
 password.addEventListener('input', (e) => {
-    const input = e.target.value
-    const lenght = input.lenght
-    const blurValue = 20 - length * 2
-    background.style.filter = `blur(${blurValue}px)`
+  const input = e.target.value
+  const length = input.length
+  const blurValue = 20 - length * 2
+  background.style.filter = `blur(${blurValue}px)`
 })
